fc3c691973bceecb684759430e45ccf2cdc07fcc
arfanlive <60953308+arfanlive@users.noreply.github.com>
2020-02-19 10:32:50 +0600

    Update maze.py

--- maze.py
+++ maze.py
@@ -3,14 +3,22 @@ import numpy as np
 rows=int(input("Enter Rows: "))
 cols=int(input("Enter Columns: "))
 rarray = np.random.randint(2,size=(rows,cols))
+f = open("maze.txt", "w")
+
 print("\nThis is your randomly generated array\n\n", rarray)
+print("\nThis is your randomly generated array\n\n", file=f)
+print(rarray, file=f)
+
+
 # A utility function to print solution matrix sol 
 def printSolution( sol ): 
 	
 	for i in sol: 
 		for j in i: 
-			print(str(j) + " ", end ="") 
-		print("") 
+			print(str(j) + " ", end ="")
+			print(str(j)+ " ", end ="", file=f)
+		print("")
+		print("", file=f)
 
 # A utility function to check if x, y is valid 
 # index for rows * cols Maze 
@@ -31,13 +39,16 @@ def solveMaze( maze ):
 	sol = [ [ 0 for j in range(cols) ] for i in range(rows) ] 
 	
 	if solveMazeUtil(maze, 0, 0, sol) == False: 
-		print("\nSolution doesn't exist"); 
+		print("\nSolution doesn't exist")
+		print("\nSolution doesn't exist", file=f) 
 		return False
 	if maze[0][0]==0:
 	    print("\nNo Entry point, Solution doesn't exist")
+	    print("\nNo Entry point, Solution doesn't exist", file=f)
 	    return False
 	
 	print ("\nYour path out is given below\n")
+	print ("\nYour path out is given below\n", file=f)
 	printSolution( sol ) 
 	return True
 	
@@ -72,10 +83,13 @@ def solveMazeUtil(maze, x, y, sol):
 if __name__ == "__main__": 
 	# Initialising the maze 
 	
-	maze = rarray #[[1, 1, 1, 1, 1], 
+	maze = rarray#[[1, 1, 1, 1, 1], 
 			#[1, 0, 0, 0, 0], 
 			#[1, 1, 1, 1, 1], 
 			#[0, 1, 0, 0, 1],
-			#[1, 1, 1, 0, 1]]
+			#[1, 1, 1, 0, 1]]#rarray 
+	        
 			
 	solveMaze(maze) 
+
+f.close()
